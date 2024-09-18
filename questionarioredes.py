2de8fa33f911853a1b7722002e4bf1e050d0d122
amanda <amandafranceschidamo@gmail.com>
2024-09-18 14:31:02 -0300

    primeiro commit

---
+++ questionarioredes.py
@@ -0,0 +1,50 @@
+print('Bem vindos ao questionário sobre redes, escolha uma alternativa e veja se está correta.')
+
+print('Vamos começar!')
+
+print('Assinale qual alternativa corresponde ao local que usamos o cabo de Rede "T568A"')
+print('a) Usado em companhias telefônicas')
+print('b) Usado em ambiente corporativos e domésticos')
+print('Usado em ambos os ambientes')
+
+resposta = input('digite uma das opções')
+
+if resposta == 'a' :
+    print('resposta certa')
+if resposta == 'b' :
+    print('resposta errada')
+if resposta == 'c' :
+    print ('resposta errada')
+
+print('Qual a sequência de cores do cabo de Ethernet?')
+print('a)Branco+verde, verde, branco+marrom, azul, laranja, branco+azul, marrom, branco+laranja')
+print('b)Branco+verde, verde, branco+azul, laranja, branco+laranja, azul, branco+marrom, marrom')
+print('c)Branco+azul, marrom, branco+verde, laranja, branco+marrom,verde, branco+laranja, marrom')
+
+resposta = input ('digite uma das opções')
+
+if resposta == 'a':
+    print('resposta errada')
+if resposta == 'b' :
+    print('resposta certa')
+if resposta == 'c' :
+    print ('resposta errada')
+    
+print('Qual a taxa máxima de transferência de um cabo Gigabit ')
+print('a)10Mb/s')
+print('b)50Mb/s')
+print('c)250Mb/s')
+print('d)1000Mb/s')
+
+resposta = input ('digite uma das opções')
+
+if resposta == 'a' :
+    print('resposta errada')
+if resposta == 'b' :
+    print('resposta errada')
+if resposta == 'c' :
+    print('resposta errada')
+if resposta == 'd' :
+    print('resposta certa')
+
+    
